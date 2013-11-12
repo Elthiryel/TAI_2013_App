1f68542e5dc51025ad0462b66e222a55ac38be36
Piotr Szmigielski <piti.pco@gmail.com>
2013-11-12 01:48:31 +0100

    Added test Dropbox authentication.

--- dropbox-integration/src/main/java/pl/edu/agh/tai/dropbox/integration/component/RegisterLayout.java
+++ dropbox-integration/src/main/java/pl/edu/agh/tai/dropbox/integration/component/RegisterLayout.java
@@ -1,14 +1,22 @@
 package pl.edu.agh.tai.dropbox.integration.component;
 
 import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import pl.edu.agh.tai.dropbox.integration.bean.SessionData;
 import pl.edu.agh.tai.dropbox.integration.model.User;
 
+import com.dropbox.core.DbxAppInfo;
+import com.dropbox.core.DbxRequestConfig;
+import com.dropbox.core.DbxSessionStore;
+import com.dropbox.core.DbxStandardSessionStore;
+import com.dropbox.core.DbxWebAuth;
 import com.vaadin.data.fieldgroup.BeanFieldGroup;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
@@ -54,7 +62,17 @@ public class RegisterLayout extends FormLayout {
 		
 		//TODO Add getting dropobox token
 		// UI.getCurrent().getPage().setLocation("http://www.dropbox.com");
-
+		DbxRequestConfig requestConfig = new DbxRequestConfig("text-edit/0.1", null);
+		// APP_KEY and APP_SECRET should come from static class probably, hardcoded here just for tests
+		DbxAppInfo appInfo = new DbxAppInfo("tog0pn54fns1v07", "bag11rqa652hg1s");
+		String redirectUri = "http://localhost:8080/DropboxIntegration/#!token";
+		ServletRequestAttributes attr = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
+		HttpSession session = attr.getRequest().getSession(true);
+		String sessionKey = "dropbox-auth-csrf-token";
+		DbxSessionStore csrfTokenStore = new DbxStandardSessionStore(session, sessionKey);
+		DbxWebAuth webAuth = new DbxWebAuth(requestConfig, appInfo, redirectUri, csrfTokenStore);
+		String authUrl = webAuth.start();
+		UI.getCurrent().getPage().setLocation(authUrl);
 	}
 
 	private void initUserLayout() {
