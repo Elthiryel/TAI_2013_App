fdbc1458a89734d718032dab23fb282f9bd89cc0
konrad <jakubiec@student.agh.edu.pl>
2013-11-26 23:11:47 +0100

    Changed download and upload method

--- dropbox-integration/src/main/java/pl/edu/agh/tai/dropbox/integration/bean/DropboxManager.java
+++ dropbox-integration/src/main/java/pl/edu/agh/tai/dropbox/integration/bean/DropboxManager.java
@@ -21,8 +21,8 @@ import com.dropbox.core.DbxRequestConfig;
 import com.dropbox.core.DbxWriteMode;
 
 /**
- * Bean class for dropbox operation.
- * Simple wrap DbxClient for required operations
+ * Bean class for Dropbox operations.
+ * Simple wrapper of DbxClient for required operations.
  * @author konrad
  *
  */
@@ -32,18 +32,18 @@ import com.dropbox.core.DbxWriteMode;
 public class DropboxManager {
 	
 	/**
-	 * Registered application name
+	 * registered application name
 	 */
 	private static final String APP ="TAIApp/1.0";
 	
 	/**
-	 * Dropobox client
+	 * Dropbox client
 	 */
 	private DbxClient client;
 	
 	/**
-	 * Initialize dropbox client
-	 * @param sessionData injected sessionData for user dropbox token
+	 * Initializes Dropbox client.
+	 * @param sessionData injected sessionData for user Dropbox token
 	 */
 	@Autowired
 	public DropboxManager(SessionData sessionData) {
@@ -52,18 +52,30 @@ public class DropboxManager {
 				.getDropboxToken());
 	}
 	
-
+	/**
+	 * Gets all files from Dropbox root directory.
+	 * @return files from Dropbox root directory
+	 * @throws DbxException
+	 */
 	public Collection<DropboxFile> getFiles() throws DbxException {
 		DropboxFile root = new DropboxFile("/", client);
 		return root.getChildren();
 	}
-
-	public File downloadFile(DropboxFile file) throws DbxException, IOException {
-		File outputFile = new File(file.getName());
+	
+	public File downloadFile(File outputFile, DropboxFile file) throws DbxException, IOException {
 		client.getFile(file.getPath(), null, new FileOutputStream(outputFile));
 		return outputFile;
 	}
 
+	/**
+	 * Uploads file to Dropbox.
+	 * @param inputFile java.io.File instance containing uploaded file
+	 * @param parent parent Dropbox directory
+	 * @return uploaded Dropbox file
+	 * @throws FileNotFoundException
+	 * @throws DbxException
+	 * @throws IOException
+	 */
 	public DropboxFile uploadFile(File inputFile, DropboxFile parent)
 			throws FileNotFoundException, DbxException, IOException {
 		DropboxFile uploadedFile;
@@ -71,18 +83,24 @@ public class DropboxManager {
 		String fileName = inputFile.getName();
 		
 		
-		if (parent != null)
+		if (parent != null && !parent.getPath().equals("/"))
 			path = parent.getPath() + "/" + fileName;
 		else
 			path = "/" + fileName;
 
-		client.uploadFile(path, DbxWriteMode.add(), inputFile.length(),
+		com.dropbox.core.DbxEntry.File result = client.uploadFile(path, DbxWriteMode.add(), inputFile.length(),
 				new FileInputStream(inputFile));
-		uploadedFile = new DropboxFile(path, client);
+		uploadedFile = new DropboxFile(result.path, client);
 		uploadedFile.setParent(parent);
+		
 		return uploadedFile;
 	}
 
+	/**
+	 * Removes file from Dropbox.
+	 * @param file removed file
+	 * @throws DbxException
+	 */
 	public void deleteFile(DropboxFile file) throws DbxException {
 		client.delete(file.getPath());
 	}
